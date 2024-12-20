c3581a937e3c98291dbd83fcd3dabf356705ef6e
Your Name <you@example.com>
2024-12-20 11:44:56 +0530

    commited

---
+++ variable.py
@@ -0,0 +1,21 @@
+# initialising the variable 
+a=10 
+b=90 
+print(a,b)
+# assigning the multiple variable within single line 
+c,d=30,4
+print(c)
+print(d)
+
+
+# data type 
+
+
+a=10 
+print(type(a)) 
+b='cloud'
+print(type(b))
+c=1.3
+print(type(c))
+d=False 
+print(type(d))
