b3b3b701f93c607325d41b68a38689016dad7ec6
Your Name <you@example.com>
2024-12-24 12:09:19 +0530

    functions

---
+++ function.py
@@ -0,0 +1,29 @@
+# function: it is set of block of code which will exicute 
+            # whenever you call the function 
+# 2 types 
+    # inbulit function: append(),pop(),remove() etc
+    # user defined:
+    #    2 types 
+    #      zero parametraised
+    #      parametraised 
+
+
+# syntax:   def fun_name(para1,para2,...):
+#               statements     
+
+def demo(): # declartion # zero parametraised function
+    a=10    # defination
+    b=20
+    print("addition",a+b)
+    
+demo()
+
+def demo1(a,b): # parametraised function 
+    print("addition of a and b=",a+b)
+
+
+demo1(3,5)
+
+# Note : while calling the parametraised function no of arguments 
+#      shuld be passed as no of parameter is declared in the Function    
+    
