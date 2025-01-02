6abd47ff2576325a1b4fe0857f9081d3765c03b4
Your Name <you@example.com>
2025-01-02 12:37:11 +0530

    cs

---
+++ controlstatements.py
@@ -0,0 +1,24 @@
+#control Statement:which controls the  flow of exicution of the code 
+# 2 types 
+  # conditional statement-->  if else , if elif
+  # looping statement --> for, while 
+  
+# Syntax:
+#     if condtion :
+#         statements 
+#     else:
+        #  Statement
+# a=True       
+# if a==False:
+#     print(" hello")
+# else :
+#     print("world")
+    
+    
+x=90
+y=80
+
+if x<y:  #false 
+    print("x is grater than y",x)
+else :
+    print("y is less than x",y)
