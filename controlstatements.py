09c7fd6561aa319ba1b0b1b3b5c4bd33d4d2769b
Your Name <you@example.com>
2025-01-03 12:15:59 +0530

    cs

--- controlstatements.py
+++ controlstatements.py
@@ -18,7 +18,44 @@
 x=90
 y=80
 
-if x<y:  #false 
-    print("x is grater than y",x)
+# if x<y:  #false 
+#     print("x is grater than y",x)
+# else :
+#     print("y is less than x",y)
+    
+
+# if condition:
+#   statement
+# elif condition:
+#   statement
+# elif condition:
+#   statement
+# else:
+#   statement
+
+if x<y:  # false 
+  print("x is less than y")
+elif x>=y: # TRue
+  print("x is equls to y")
+elif x!=y:  # true 
+  print("x is not equls to y")
 else :
-    print("y is less than x",y)
+  print("none")
+  
+print("==============   =============")  
+
+
+# we cant use the if condition multiple times to check the multile condition As it gives the 75% result 
+if x<y:  # false 
+  print("x is less than y")
+if x>=y: # true
+     print("x is equls to y")
+if x!=y:  # true 
+  print("x is not equls to y")
+else :                                      
+  print("none")
+
+  
+ 
+
+
